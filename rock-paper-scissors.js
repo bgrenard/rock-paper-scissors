f877e8f89bf0dcb85670812cfe1469ace29b4ec6
Bill Grenard <williamgrenard@gmail.com>
2024-02-04 16:53:12 -0800

    Add function to play a single game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,27 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["Paper", "Rock", "Scissors"];
     let randIndex = Math.floor( Math.random()*3 );
     return choices[randIndex];
 }
 
-// console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    let firstLetter = playerSelection.charAt(0).toUpperCase();
+    let remainingLetters = playerSelection.slice(1).toLowerCase();
+    let playerCapitalized = firstLetter + remainingLetters;
+
+    if (playerCapitalized === computerSelection) {
+        return `You Tied! You and the computer both chose ${playerCapitalized}!`
+    }
+    else if (
+            playerCapitalized === "Rock" && computerSelection === "Paper" ||
+            playerCapitalized === "Paper" && computerSelection === "Scissors" ||
+            playerCapitalized === "Scissors" && computerSelection === "Rock"
+        ) {
+        return `You Lose! ${computerSelection} beats ${playerCapitalized}.`
+    }
+    else {
+        return `You Win! ${playerCapitalized} beats ${computerSelection}.`
+    }
+}
+
+console.log(playRound("paper", "Rock"))
