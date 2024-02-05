761e4607c0c44507811be797f55068fa6df04189
Bill Grenard <williamgrenard@gmail.com>
2024-02-04 16:23:32 -0800

    Create function to randomly choose rock paper scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randIndex = Math.floor( Math.random()*3 );
+    return choices[randIndex];
+}
+
+// console.log(getComputerChoice())
